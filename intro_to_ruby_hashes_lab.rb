35b219b926187f424555fe0319538620728402b6
Veronica Kim <vero.a.kim@gmail.com>
2018-01-30 22:49:14 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,12 @@ end
 def monopoly_with_third_tier
   monopoly = {:railroads => {
     :pieces=>4,
-    :names=>{},
+    :names=>{
+      :reading_railroad=>{"mortgage_value"=>"$100"}
+      :pennsylvania_railroad=>{"mortgage_value"=>"$200"}
+      :b_and_o_railroad=>{"mortgage_value"=>"$400"}
+      :shortline=>{"mortgage_value"=>"$800"}
+    },
     :rent_in_dollars=>{
       :one_piece_owned=>25,
       :two_pieces_owned=>50,
